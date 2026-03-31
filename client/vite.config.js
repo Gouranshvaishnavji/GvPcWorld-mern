f02bb152f8e89134ccd0c7c31a5520fcf5694b4c
Gouranshvaishnavji <gouransh.v@kalvium.community>
2026-03-31 10:01:28 +0530

    fixed terser issue trying to find better alternate

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,12 +5,7 @@ export default defineConfig({
   plugins: [react()],
   publicDir: 'src/assets',
   build: {
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true
-      }
-    },
+    minify: 'esbuild',
     rollupOptions: {
       output: {
         manualChunks: {
