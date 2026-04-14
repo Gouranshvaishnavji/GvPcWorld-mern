d4c113beab3765f2d972a01e9c1b9ef9c9a10e88
Gouranshvaishnavji <gouransh.v@kalvium.community>
2026-04-14 23:16:02 +0530

    save changes
    we are now moving to next part of this project that is using design principles to make it more than crud

--- server/middleware/errorHandler.js
+++ server/middleware/errorHandler.js
@@ -3,7 +3,8 @@ import { logger } from '../util/logger.js';
 export default function errorHandler(err, req, res, next) {
   logger.error(`${err.message} - ${req.method} ${req.originalUrl}`);
 
-  const statusCode = err.statusCode || 500;
+  const statusCode = err.statusCode || 500; //200: success,201: created,202: accepted, 400: bad request, 401: unauthorized, 403: forbidden, 404: not found, 500: internal server error, 502: bad gateway, 503: service unavailable, 504: gateway timeout
+
   const message = err.message || 'Internal Server Error';
 
   res.status(statusCode).json({
